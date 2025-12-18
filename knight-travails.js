75d2c8268e04712585d5db8f7999daa4e49df06b
javgiu <javypicallo@gmail.com>
2025-12-17 22:43:54 -0500

    feat: create displayPath and separate getValidMoves
    displayPath displays the path just like the lesson does
    getValidMoves separate the logic of getting the valid moves making the code more legible

--- knight-travails.js
+++ knight-travails.js
@@ -1,4 +1,6 @@
-// const board = createChestBoard();
+function positionToKey(position) {
+  return `${position[0]}, ${position[1]}`;
+}
 
 function knightMoves(fromVertex, toVertex) {
   const possibleMoves = [
@@ -11,62 +13,41 @@ function knightMoves(fromVertex, toVertex) {
     [-2, 1],
     [-2, -1],
   ];
-
-  const x = fromVertex[0];
-  const y = fromVertex[1];
-
-  const queue = [[x, y, [[x, y]]]];
+  const queue = [[...fromVertex, [fromVertex]]];
   const set = new Set();
 
   while (queue.length > 0) {
     const position = queue.shift();
-    if (`${position[0]}, ${position[1]}` === `${toVertex[0]}, ${toVertex[1]}`) {
+    if (positionToKey(position) === positionToKey(toVertex)) {
       return [...position[2]];
     } else {
-      set.add(`${position[0]}, ${position[1]}`);
-      possibleMoves.forEach((move) => {
-        const newX = position[0] + move[0];
-        const newY = position[1] + move[1];
-        if (newX >= 0 && newY >= 0 && newX < 8 && newY < 8) {
-          if (set.has(`${newX}, ${newY}`)) return;
-          else queue.push([newX, newY, [...position[2], [newX, newY]]]);
+      set.add(positionToKey(position));
+      const validMoves = getValidMoves(position[0], position[1]);
+      validMoves.forEach((move) => {
+        if (!set.has(positionToKey(move))) {
+          queue.push([...move, [...position[2], move]]);
         }
       });
     }
   }
+
+  function getValidMoves(x, y) {
+    const edges = [];
+    possibleMoves.forEach((move) => {
+      const newX = x + move[0];
+      const newY = y + move[1];
+      if (newX >= 0 && newY >= 0 && newX < 8 && newY < 8) {
+        edges.push([newX, newY]);
+      }
+    });
+
+    return edges;
+  }
 }
 
-console.log(knightMoves([2, 1], [7, 7]));
+function displayPath(path) {
+  console.log(`You made it in ${path.length - 1} moves! Here's your path: `);
+  path.forEach((pos) => console.log(pos));
+}
 
-// function createChestBoard() {
-//   const chestBoard = [];
-//   const boardSize = 8;
-//   for (let i = 0; i < boardSize; i++) {
-//     chestBoard.push([]);
-//     for (let j = 0; j < boardSize; j++) {
-//       if ((i + j) % 2) chestBoard[i].push(createTile([i, j], "white"));
-//       else chestBoard[i].push(createTile([i, j], "black"));
-//     }
-//   }
-//   return chestBoard;
-// }
-
-// function createTile(tileCoordinates, tileColor) {
-//   const coordinates = tileCoordinates;
-//   const visited = 0;
-//   const color = tileColor;
-//   return { coordinates, visited, color };
-// }
-
-// function that returns the 8 possible edges from a node in the board
-
-// function getEdges(vertex) {
-//   const x = vertex[0];
-//   const y = vertex[1];
-//   const tile = board[x][y];
-//   const edges = [];
-
-//   return edges;
-// }
-
-// console.log(getEdges([, 0]));
+displayPath(knightMoves([2, 1], [7, 7]));
